32eb236fb54d4c78a341dd1e4a04bda04d77db19
OZAWA <ozawa@ozwk.net>
2025-01-24 02:03:31 +0900

    add temp set func

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,50 @@
-export default {
-	async fetch (request: Request, env: Env) {
-		const { pathname } = new URL(request.url);
-		//TODO 温度セットの記述をする
-		//TODO honoにする
-		if (pathname === '/') {
-			let data = await env.DB.prepare(
-				`
-       			SELECT * FROM temperature
-        		ORDER BY unixtime DESC
-        		LIMIT 1
-				`
-			).all();
+import { Hono } from 'hono';
+import { basicAuth } from 'hono/basic-auth';
 
-			let date = new Date(data.results[0].unixtime * 1000);
-			date.setHours(date.getHours() + 9);
-			return Response.json(
-				`${date.getHours().toString().padStart(2, '0')}時${date.getMinutes().toString().padStart(2, '0')}分現在の部屋の温度は${
-					data.results[0].temperature
-				}℃です`
-			);
-		}
+const app = new Hono();
+
+app.get('/', async c => {
+	let data = await c.env.DB.prepare(
+		`
+	   		SELECT * FROM temperature
+	    	ORDER BY unixtime DESC
+	    	LIMIT 1
+		`
+	).all();
+
+	let date = new Date(data.results[0].unixtime * 1000);
+	date.setHours(date.getHours() + 9);
+
+	return c.text(
+		`${date.getHours().toString().padStart(2, '0')}時${date.getMinutes().toString().padStart(2, '0')}分現在の部屋の温度は${
+			data.results[0].temperature
+		}℃です`
+	);
+});
+
+app.use('/set', async (c, next) => {
+	const auth = basicAuth({
+		username: c.env.SECRET_USERNAME,
+		password: c.env.SECRET_PASSWORD
+	});
+	return auth(c, next);
+});
+
+app.post('/set', async c => {
+	try {
+		const formData = await c.req.formData();
+		await c.env.DB.prepare(
+			`
+    			insert into temperature (unixtime, temperature) values (?, ?)
+  			`
+		)
+			.bind(formData.get('unixtime'), formData.get('temperature'))
+			.run();
+	} catch (e) {
+		c.status(500);
+		return c.text('SET ERROR');
 	}
-};
+	return c.text('SET OK');
+});
+
+export default app;
