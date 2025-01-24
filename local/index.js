40a2d38f454b806befe9bb13dca5454f845ac09b
OZAWA <ozawa@ozwk.net>
2025-01-25 06:14:08 +0900

    fix cron settings

--- local/index.js
+++ local/index.js
@@ -1,13 +1,13 @@
 const cron = require('node-cron');
 require('dotenv').config()
 
-cron.schedule('* */3 * * *', () => {
+cron.schedule('*/3 * * * *', () => {
     let unixtime = new Date();
     unixtime.setHours(unixtime.getHours() + 9);
     const temperature = Math.floor(Math.random() * 1000)
     const myHeaders = new Headers();
     const formdata = new FormData();
-    myHeaders.append("Authorization", "Basic "+SECRET_ENCORDED_USR_AND_PW);
+    myHeaders.append("Authorization", "Basic "+process.env.SECRET_ENCORDED_USR_AND_PW);
     formdata.append("unixtime", unixtime.getTime());
     formdata.append("temperature", temperature);
     const requestOptions = {
